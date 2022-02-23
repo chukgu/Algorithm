58972879ffd37782c5bf001e39c304648d08c7fb
chukgu <chukgu1643@gmail.com>
2022-02-23 22:43:13 +0900

    2022-02-23 22:43

---
+++ LeetCode/5_Longest Palindromic Substring.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+
+
+        return s
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestPalindrome(s = "babad"))
+    print(s.longestPalindrome(s = "cbbd"))
+    print(s.longestPalindrome(s = "a"))
+    print(s.longestPalindrome(s = "ac"))
